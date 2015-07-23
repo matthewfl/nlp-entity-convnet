5a4806d9107b173c39c42c938eab9607b1d0c64d
Matthew Francis-Landau <matthew@matthewfl.com>
2015-07-22 22:24:14 -0700

    fck

--- sentiment_sents.py
+++ sentiment_sents.py
@@ -15,7 +15,7 @@ class Sentiment(object):
         test_size = len(data) // 5
         train, test = data[test_size:], data[:test_size]
         self.train_X, self.train_Y = self._split(train)
-        self.test_X, self.test_Y = self._split(train)
+        self.test_X, self.test_Y = self._split(test)
 
     def _split(self, x):
         return map(lambda a: a[0], x), map(lambda a: a[1], x)

a3c529a78722632738b32425c5b2a06eefcf72ec
Matthew Francis-Landau <matthew@matthewfl.com>
2015-07-25 00:05:47 -0700

    using simple maxing layer now

--- helpers.py
+++ helpers.py
@@ -1,65 +1,17 @@
 import theano.tensor as T
 import lasagne
+from lasagne.layers.base import Layer
 import theano
 
 
-def create_iter_functions(dataset, output_layer,
-                          X_tensor_type=T.matrix,
-                          batch_size=100,
-                          learning_rate=.01, momentum=.9):
-    """Create functions for training, validation and testing to iterate one
-       epoch.
-    """
-    batch_index = T.iscalar('batch_index')
-    X_batch = X_tensor_type('x')
-    y_batch = T.ivector('y')
-    batch_slice = slice(batch_index * batch_size,
-                        (batch_index + 1) * batch_size)
+class SimpleMaxingLayer(Layer):
 
-    output = lasagne.layers.get_output(output_layer, X_batch)
-    loss_train = lasagne.objectives.categorical_crossentropy(output, y_batch)
-    loss_train = loss_train.mean()
+    def __init__(self, incoming,  **kwargs):
+        super(SimpleMaxingLayer, self).__init__(incoming, **kwargs)
 
-    output_test = lasagne.layers.get_output(output_layer, X_batch,
-                                            deterministic=True)
-    loss_eval = lasagne.objectives.categorical_crossentropy(output_test,
-                                                            y_batch)
-    loss_eval = loss_eval.mean()
+    def get_output_shape_for(self, input_shape):
+        r = (input_shape[0], input_shape[1], 1)
+        return r
 
-    pred = T.argmax(output_test, axis=1)
-    accuracy = T.mean(T.eq(pred, y_batch), dtype=theano.config.floatX)
-
-    all_params = lasagne.layers.get_all_params(output_layer)
-    updates = lasagne.updates.nesterov_momentum(
-        loss_train, all_params, learning_rate, momentum)
-
-    iter_train = theano.function(
-        [batch_index], loss_train,
-        updates=updates,
-        givens={
-            X_batch: dataset['X_train'][batch_slice],
-            y_batch: dataset['y_train'][batch_slice],
-        },
-    )
-
-    # iter_valid = theano.function(
-    #     [batch_index], [loss_eval, accuracy],
-    #     givens={
-    #         X_batch: dataset['X_valid'][batch_slice],
-    #         y_batch: dataset['y_valid'][batch_slice],
-    #     },
-    # )
-
-    iter_test = theano.function(
-        [batch_index], [loss_eval, accuracy],
-        givens={
-            X_batch: dataset['X_test'][batch_slice],
-            y_batch: dataset['y_test'][batch_slice],
-        },
-    )
-
-    return dict(
-        train=iter_train,
-        # valid=iter_valid,
-        test=iter_test,
-    )
+    def get_output_for(self, input, **kwargs):
+        return T.max(input, axis=2)
---
+++ sentiment_exp.py
@@ -0,0 +1,120 @@
+class SentimentExp(object):
+
+    def __init__(self, train_X, train_Y, wordvecs=wordvectors):
+        self.train_X = train_X
+        self.train_Y = train_Y
+        self.wordvecs = wordvecs
+
+        self.input_size = 10  # not used
+        self.batch_size = 50
+
+        self.learning_rate = .01
+        self.momentum = .9
+
+        #self.train_X_rep = np.array([[self.getRep(x)] for x in self.train_X])
+        self.train_X_rep = np.array([wordvecs.tokenize(x) for x in self.train_X])
+
+        self._setup()
+
+    def getRep(self, sent):
+        ret = []
+        for i in xrange(self.input_size):
+            if i < len(sent):
+                ret.append(self.wordvecs[sent[i]])
+            else:
+                ret.append(np.zeros(self.wordvecs.vector_size))
+        return np.matrix(ret).reshape((1, self.input_size, self.wordvecs.vector_size))
+
+    def _setup(self):
+        self.x_batch = T.imatrix('x')
+        self.y_batch = T.ivector('y')
+
+        self.input_l = lasagne.layers.InputLayer((None, 50))
+
+        self.embedding_l = EmbeddingLayer(
+            self.input_l,
+            W=self.wordvecs.get_numpy_matrix(),
+            add_word_params=False,
+        )
+
+        self.first_l = lasagne.layers.Conv2DLayer(
+            self.embedding_l,
+            num_filters=100,
+            filter_size=(2, self.wordvecs.vector_size),
+            name='conv1',
+            nonlinearity=lasagne.nonlinearities.tanh,
+        )
+
+        self.first_l_max = lasagne.layers.MaxPool2DLayer(
+            self.first_l,
+            pool_size=(1,9)
+        )
+
+        self.hidden1_l = lasagne.layers.DenseLayer(
+            self.first_l_max,
+            num_units=50,
+            nonlinearity=lasagne.nonlinearities.tanh,
+        )
+
+        self.hidden1_l_drop = lasagne.layers.DropoutLayer(
+            self.hidden1_l,
+            p=.25,
+        )
+
+        self.out_l = lasagne.layers.DenseLayer(
+            self.hidden1_l_drop,
+            num_units=1,
+            nonlinearity=lasagne.nonlinearities.tanh,
+        )
+
+        self.output = lasagne.layers.get_output(self.out_l, self.x_batch)
+
+        self.loss_vec_old = (self.output.reshape((self.output.size,)) - self.y_batch) ** 2
+        self.output_diff = T.neq((self.output.flatten() > .5),(self.y_batch > .5)).sum()
+        self.loss_vec = lasagne.objectives.binary_crossentropy(T.clip(self.output.reshape((self.output.size,)), .01, .99), self.y_batch)
+
+        self.all_params = lasagne.layers.get_all_params(self.out_l)
+
+        self.updates = lasagne.updates.adagrad(self.loss_vec.mean(), self.all_params, .01)
+        #self.updates = lasagne.updates.apply_momentum(self.updates_adagrad)
+
+        self.train_func = theano.function(
+            [self.x_batch, self.y_batch],
+            [self.loss_vec.mean(), self.loss_vec],
+            updates=self.updates,
+        )
+
+        self.loss_func = theano.function(
+            [self.x_batch, self.y_batch],
+            [self.loss_vec.sum(), self.loss_vec, self.output_diff],
+        )
+
+    def _make_zero(self):
+        self.embedding_l.W.get_value(borrow=True)[0,:] = 0
+
+    def train(self):
+        for s in xrange(0, len(self.train_X_rep), self.batch_size):
+            end = s + self.batch_size
+            if end > len(self.train_X_rep):
+                end = len(self.train_X_rep)
+            X_vals = np.array(self.train_X_rep[s:end]).astype('int32')
+            y_vals = np.array(self.train_Y[s:end]).astype('int32')
+            loss, _ = self.train_func(X_vals, y_vals)
+            self._make_zero()
+
+    def test_loss(self, test_X, test_Y):
+        test_X_rep = np.array([self.wordvecs.tokenize(x) for x in test_X])
+        loss_sum = 0.0
+        wrong = 0.0
+        for s in xrange(0, len(test_X_rep), self.batch_size):
+            end = s + self.batch_size
+            if end > len(self.train_X_rep):
+                end = len(self.train_X_rep)
+            X_vals = np.array(self.train_X_rep[s:end]).astype('int32')
+            y_vals = np.array(self.train_Y[s:end]).astype('int32')
+            loss, _, output_diff = self.loss_func(X_vals, y_vals)
+            wrong += output_diff
+            loss_sum += loss
+        return loss_sum / len(test_X_rep), wrong / len(test_X_rep)
+
+experiment = SentimentExp(sentiment.train_X, sentiment.train_Y)
